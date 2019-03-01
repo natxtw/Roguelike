4b342437d1e6776852f6cf24b4d02315e21c2bcc
natxtw <36550708+natxtw@users.noreply.github.com>
2019-03-01 12:53:09 +0000

    Add files via upload
    In this patch i've attempted to use some threading.

--- Roguelike-master/Roguelike/main.cpp
+++ Roguelike-master/Roguelike/main.cpp
@@ -1,16 +1,47 @@
 #include <iostream>
 #include <Game.h>
 #include <TcpNetwork.h>
+#include <thread>
+#include <vector>
 
 using namespace std;
+
+struct DataStorage
+{
+    void ParamFunct(int Param1, int Param2, int Param3);
+};
+
+void randomfunct()
+{
+        std::cout << " Function thread " << std::this_thread::get_id() << std::endl;
+}
 
 int main()
 {
     //Game game;
     //game.run();
 
-    TcpNetwork network;
-    network.TcpStartUp();
+    //TcpNetwork network;
+    //network.TcpStartUp();
+
+/*    std::thread FirstThread(&randomfunct); //Within the brackets is the entry point
+      std::cout << " Main thread " << std::endl;
+      FirstThread.join(); //Waits to end the thread
+*/
+    DataStorage MyStruct;
+
+    std::vector<std::thread> VecThread;
+
+    for (int i = 0; i < 10; i++)
+    {
+        VecThread.push_back(std::thread(randomfunct));
+    }
+
+    for (auto& thread_ : VecThread)
+    {
+        thread_.join();
+    }
+
 
     return 0;
 }
